e3fc3037926d53e9556afd5d253b29e3ba4b285b
unknown <bvm.caicedo@yavirac.edu.ec>
2023-11-24 20:46:05 -0500

    Profesor

--- web/src/app/Profesor/formulario-libro/formulario-libro.component.ts
+++ web/src/app/Profesor/formulario-libro/formulario-libro.component.ts
@@ -1,8 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { LibrosService } from '../libro.service';
-
-
 
 @Component({
   selector: 'app-formulario-libro',
@@ -16,12 +15,18 @@ export class FormularioLibroComponent {
 
   constructor(
     private formBuilder: FormBuilder,
-    private libroService: LibrosService
+    private libroService: LibrosService,
+    private router: Router
   ) {
     this.libroForm = this.formBuilder.group({
       titulo: ['', Validators.required],
       autor: ['', Validators.required],
-      descripcion: ['', Validators.required]
+      descripcion: ['', Validators.required],
+      fechaPublicacion: ['', Validators.required],
+      paginas: ['', Validators.required],
+      materia: ['', Validators.required],
+      carreras: ['', Validators.required],
+      imagenUrl: ['', Validators.required]
     });
   }
 
@@ -29,16 +34,25 @@ export class FormularioLibroComponent {
     if (this.libroForm.invalid) {
       return;
     }
-  
+
     const libro = {
       id: new Date().getTime(),
       titulo: this.libroForm.value.titulo,
       autor: this.libroForm.value.autor,
-      descripcion: this.libroForm.value.descripcion
+      descripcion: this.libroForm.value.descripcion,
+      fechaPublicacion: this.libroForm.value.fechaPublicacion,
+      paginas: this.libroForm.value.paginas,
+      materia: this.libroForm.value.materia,
+      carreras: this.libroForm.value.carreras,
+      imagenUrl: this.libroForm.value.imagenUrl
     };
-  
+
     this.libroService.agregarLibro(libro);
     this.libroForm.reset();
     this.libroAgregado.emit();
+
+    // Mostrar mensaje y navegar a la pestaña del catálogo
+    alert('Libro agregado');
+    this.router.navigate(['/catalogo']);
   }
 }
