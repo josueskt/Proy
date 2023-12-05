67e756725250fb4afc9aeab67592a5dbd3d8d2f6
Ramen <josueskt22@hotmail.com>
2023-12-04 20:24:56 -0500

    asdad

--- mobil/lib/main.dart
+++ mobil/lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner:  false,
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
       home: SafeArea(
         child: Scaffold(
@@ -16,38 +16,50 @@ class MyApp extends StatelessWidget {
             backgroundColor: const Color.fromRGBO(97, 94, 144, 0.8),
             title: const Text('Biblioteca'),
           ),
-          body:  Container(
-            decoration: const BoxDecoration(image: DecorationImage(image: NetworkImage('https://img.freepik.com/vector-gratis/interior-biblioteca-sala-vacia-leer-libros-estantes-madera_33099-1722.jpg?size=626&ext=jpg&ga=GA1.1.1803636316.1701475200&semt=ais'),fit: BoxFit.cover,)),
+          body: Container(
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+              image: NetworkImage(
+                  'https://img.freepik.com/vector-gratis/interior-biblioteca-sala-vacia-leer-libros-estantes-madera_33099-1722.jpg?size=626&ext=jpg&ga=GA1.1.1803636316.1701475200&semt=ais'),
+              fit: BoxFit.cover,
+            )),
             child: Center(
-              
-              child: ListView(
-                
-                children: [
-            
-                  Container(
-                    alignment: Alignment.center ,
-                    width: 200,
-                    height: 250,
-                    child: const Text('BIBIOTECA', style: TextStyle(fontWeight:FontWeight.w200,fontSize: 50 ,color: Colors.white),),
+                child: ListView(
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  width: 200,
+                  height: 250,
+                  child: const Text(
+                    'BIBIOTECA',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w200,
+                        fontSize: 50,
+                        color: Colors.white),
                   ),
-                  TextField(
-                    textAlign: TextAlign.center,
-                   style: const TextStyle(color: Color.fromARGB(255, 255, 255, 255)), // Agregado para cambiar el color del texto
-                          decoration: InputDecoration(
-                            hintText: 'Buscar un libro',
-                            hintStyle: const TextStyle(color: Color.fromARGB(255, 255, 255, 255)), // Agregado para cambiar el color del texto de sugerencia
-                            suffixIcon: const Icon(Icons.search , size: 40, color: Colors.amber,),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(30),
-                            ),
-                          ),
-                  
-                  
-                  )
-            
-                  
-                ],
-              )),
+                ),
+                TextField(
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                      color: Color.fromARGB(255, 255, 255,
+                          255)), // Agregado para cambiar el color del texto
+                  decoration: InputDecoration(
+                    hintText: 'Buscar un libro',
+                    hintStyle: const TextStyle(
+                        color: Color.fromARGB(255, 255, 255,
+                            255)), // Agregado para cambiar el color del texto de sugerencia
+                    suffixIcon: const Icon(
+                      Icons.search,
+                      size: 40,
+                      color: Colors.amber,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                )
+              ],
+            )),
           ),
         ),
       ),
