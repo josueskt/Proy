b10ceecdd9db582055a6127b6b26faa38e85d815
Ramen <josueskt32@gmail.com>
2023-12-12 01:10:48 -0500

    asdsad

--- mobil/lib/src/pages/home.dart
+++ mobil/lib/src/pages/home.dart
@@ -2,11 +2,17 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:mobil/src/pages/libros.dart';
+import 'package:mobil/src/pages/login.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
       home: Home(),
@@ -22,33 +28,57 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  TextEditingController _controller = TextEditingController();
+  final TextEditingController _controller = TextEditingController();
   List myList = [];
 
   Future<void> fetchData(String searchString) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('token');
+
     try {
-      final response = await http.get(
-          Uri.parse('http://192.168.3.20:3000/buscador?cadena=$searchString'));
+      if (token != null) {
+        final response = await http.get(
+          Uri.parse('http://192.168.3.20:3000/buscador?cadena=$searchString'),
+          headers: {
+            'Authorization': 'Bearer $token',
+          },
+        );
 
-      if (response.statusCode == 200) {
-        setState(() {
-          final lista = (List.from(json.decode(response.body)));
-
-          Navigator.push(
+        if (response.statusCode == 200) {
+          setState(() {
+            final lista = List.from(json.decode(response.body));
+            Navigator.push(
               context,
               MaterialPageRoute(
-                  builder: (context) => Libros(
-                        datos: lista,
-                      )));
-        });
+                builder: (context) => Libros(
+                  datos: lista,
+                ),
+              ),
+            );
+          });
+        } else {
+          // Manejar error
+          print('Error en la solicitud: ${response.statusCode}');
+        }
       } else {
-        // Manejar error
-        print('Error en la solicitud: ${response.statusCode}');
+        // El token no está presente, redirigir al login o realizar alguna acción
+        // ignore: use_build_context_synchronously
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => Login(),
+          ),
+        );
       }
     } catch (e) {
       // Manejar excepción
       print('Error en la solicitud: $e');
     }
+  }
+
+  Future<void> removeTokenFromLocalStorage() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.remove('token');
   }
 
   @override
@@ -63,6 +93,30 @@ class _HomeState extends State<Home> {
             style: TextStyle(color: Colors.white),
           ),
         ),
+        actions: [
+          PopupMenuButton<String>(
+            onSelected: (value) {
+              if (value == 'eliminarToken') {
+                removeTokenFromLocalStorage();
+                // Puedes realizar otras acciones después de eliminar el token.
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Login(),
+                  ),
+                );
+              }
+            },
+            itemBuilder: (context) {
+              return [
+                PopupMenuItem(
+                  value: 'eliminarToken',
+                  child: Text('Eliminar Token'),
+                ),
+              ];
+            },
+          ),
+        ],
       ),
       body: Container(
         decoration: const BoxDecoration(
--- mobil/lib/src/pages/login.dart
+++ mobil/lib/src/pages/login.dart
@@ -1,12 +1,75 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:mobil/src/pages/home.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Login extends StatelessWidget {
-  const Login({Key? key}) : super(key: key);
+  final TextEditingController cedulaController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  Future<void> _login(BuildContext context) async {
+    final url = 'http://192.168.3.20:3000/login';
+    final response = await http.post(
+      Uri.parse(url),
+      body: {
+        'cedula': cedulaController.text,
+        'password': passwordController.text,
+      },
+    );
+
+    // La solicitud fue exitosa, puedes manejar la respuesta aquí
+
+    var values = jsonDecode(response.body);
+    var ms = values["message"];
+    if (ms != null) {
+      print(values["message"]);
+    } else if (ms == null) {
+      saveTokenToLocalStorage(values["token"]);
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => Home(),
+        ),
+      );
+    }
+  }
+
+  void saveTokenToLocalStorage(String token) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('token', token);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("login"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Flutter'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: cedulaController,
+              decoration: InputDecoration(labelText: 'Cédula'),
+            ),
+            SizedBox(height: 16),
+            TextField(
+              controller: passwordController,
+              obscureText: true,
+              decoration: InputDecoration(labelText: 'Contraseña'),
+            ),
+            SizedBox(height: 32),
+            ElevatedButton(
+              onPressed: () =>
+                  _login(context), // Pass the context to the _login method
+              child: Text('Iniciar sesión'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
