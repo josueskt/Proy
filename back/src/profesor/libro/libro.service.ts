141003936942d700ea04cef80d1dee59b4ca79c0
Ramen <josueskt32@gmail.com>
2024-01-07 02:34:05 -0500

    asd

--- back/src/profesor/libro/libro.service.ts
+++ back/src/profesor/libro/libro.service.ts
@@ -1,17 +1,23 @@
-import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { SqlService } from 'src/sql/sql.service';
+
+import * as pdfParse from 'pdf-parse';
+
 import * as fs from 'fs';
 import * as path from 'path';
-import { MessageDto } from 'src/common/message.dto';
+
 
 @Injectable()
 export class LibroService {
-  constructor(private readonly sql: SqlService) {}
 
-  async traer(nombre: string): Promise<any> {
-    //as
-    const reslut = await this.sql.query(
-      `SELECT
+    constructor(private readonly sql: SqlService) {
+
+    }
+
+    async traer(nombre: String): Promise<any> {
+        //as
+        const reslut = await this.sql.query(
+            `SELECT
             l.id_libro,
             l.titulo,
             l.year_of_publication,
@@ -30,21 +36,22 @@ export class LibroService {
             l.fk_creador = $1
         GROUP BY
             l.id_libro, l.titulo, l.year_of_publication, l.review, l.imagen, a.nombre, c.nombre, l.nombre_archivo;
-        `,
-      [nombre],
-    );
-    return reslut;
-  }
+        `
+            , [nombre])
+        return reslut
 
-  async by_id(id: number): Promise<any> {
-    try {
-      const reslut = await this.sql.query(
-        `   select
+
+    }
+
+    async by_id(id: Number): Promise<any> {
+        try {
+            const reslut = await this.sql.query(`
+            select
             l.review ,
             l.year_of_publication,
             l.id_libro ,
             l.imagen, 
-            l.nombre_archivo ,            
+            l.nombre_archivo ,
             l.titulo,
             a.nombre as autor,
             p.nombre as profesor,
@@ -52,44 +59,57 @@ export class LibroService {
             t.nombre as tipo
             from libros.libro as l right join inst.usuario as p on p.id_user = l.fk_creador
             right join libros.autor as a on l.fk_autor = a.id_autor
-            right join libros.carrera as c on l.fk_carrera = c.id_carrera   
-            where id_libro = ($1)`,
-        [id],
-      );
-      if (reslut.length === 0) {
-        return new NotFoundException(new MessageDto('No se encontró el libro'));
-      } else {
-        return reslut;
-      }
-    } catch (error) {
-      return new NotFoundException(`Error a al traer libro: ${error}`);
+            right join libros.carrera as c on l.fk_carrera = c.id_carrera  
+            LEFT JOIN libros.tipo as t on l.fk_tipo = t.id_tipo 
+            where id_libro = ($1)`, [id])
+            if (reslut.length === 0) {
+                return "no existe el id de esta carrera "
+            } else { return reslut }
+        } catch (error) {
+            return error
+        }
+
+
     }
-  }
 
-  async crear(libros, file: any): Promise<string> {
-    const libro = JSON.parse(libros);
 
-    try {
-      if (!file || !file.buffer) {
-        throw new HttpException(new MessageDto(`Error al cargar el archivo`), HttpStatus.BAD_REQUEST);
-      }     
+    async crear(libros, file: any): Promise<string> {
+        const libro = JSON.parse(libros);
+        
 
-      // Genera un nombre único para el archivo PDF
-      const uniqueFileName = `${Date.now()}-${file.originalname}`;
+        if(!libro.archivo_url){
+        try {
+            if (!file || !file.buffer) {
+                throw new HttpException('Archivo no válido', HttpStatus.BAD_REQUEST);
+            }
+            const pdfBuffer = file.buffer;
+            const data = await pdfParse(pdfBuffer);
 
-      // Construye la ruta completa del archivo en la carpeta pdfs
-      const pdfPath = path.join(process.env.Docs, uniqueFileName);
+            
 
-      // Crea el stream de escritura del archivo
-      const writeStream = fs.createWriteStream(pdfPath);
-      writeStream.write(file.buffer);
 
-      // Cierra el flujo después de escribir el contenido
-      writeStream.end();
 
-      //  Inserta los datos del libro en la base de datos, utilizando el campo nombre_archivo
-      await this.sql.query(
-        `INSERT INTO libros.libro (
+
+
+            // Genera un nombre único para el archivo PDF
+            const uniqueFileName = `${Date.now()}-${file.originalname}`;
+
+
+            // Construye la ruta completa del archivo en la carpeta pdfs
+            const pdfPath = path.join( process.env.Docs, uniqueFileName);
+
+            // Crea el stream de escritura del archivo
+            const writeStream = fs.createWriteStream(pdfPath);
+            writeStream.write(file.buffer);
+
+            // Cierra el flujo después de escribir el contenido
+            writeStream.end();
+
+
+
+
+            //  Inserta los datos del libro en la base de datos, utilizando el campo nombre_archivo
+            await this.sql.query(`INSERT INTO libros.libro (
                 titulo,
                 year_of_publication,
                 review,
@@ -102,51 +122,94 @@ export class LibroService {
                 fk_tipo,
                 codigo,
                 editorial
-                ) VALUES ($1, $2, $3, $4, $5, $6, $7,$8,$9,$10,$11,$12)`,
-        [
-          libro.titulo,
-          2004,
-          libro.descripcion,
-          libro.imagen,
-          uniqueFileName,
-          libro.isbn,
-          libro.fk_creador,
-          libro.fk_autor,
-          libro.fk_carrera,
-          libro.fk_tipo,
-          libro.codigo,
-          libro.editorial,
-        ],
-      );
-      return 'Libro creado exitosamente'
-    } catch (error) {      
-      throw new HttpException(new MessageDto(`Error al crear el libro con PDF: ${error.message}`), HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-  }
+                ) VALUES ($1, $2, $3, $4, $5, $6, $7,$8,$9,$10,$11,$12)`, [
+                libro.titulo,
+                2004,
+                libro.descripcion,
+                libro.imagen,
+                uniqueFileName,
+                libro.isbn,
+                libro.fk_creador,
+                libro.fk_autor,
+                libro.fk_carrera,
+                libro.tipo,
+                libro.codigo,
+                libro.editorial
+               
+            ]);
 
-  async eliminar(id: string) {
-    try {
-      await this.sql.query(
-        'DELETE FROM tramites.prestamo WHERE fk_libro = $1;',
-        [id],
-      );
-      await this.sql.query('DELETE FROM  libros.libro WHERE  id_libro = $1;', [
-        id,
-      ]);
-      return new MessageDto('Libro eliminado exitosamente');
-    } catch (error) {
-      return new NotFoundException(new MessageDto(`Error al eliminar el libro: ${error}`));
+            return 'Libro creado exitosamente';
+        } catch (error) {
+            console.error('Error al crear el libro con PDF:', error);
+            return error
+          //  throw new HttpException(`Error al crear el libro con PDF: ${error.message}`, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        }else
+        {
+            console.log(libro)
+
+            await this.sql.query(`INSERT INTO libros.libro (
+                titulo,
+                year_of_publication,
+                review,
+                imagen ,
+                nombre_archivo ,
+                isbn ,
+                fk_creador,
+                fk_autor,
+                fk_carrera,
+                fk_tipo,
+                codigo,
+                editorial
+                ) VALUES ($1, $2, $3, $4, $5, $6, $7,$8,$9,$10,$11,$12)`, [
+                libro.titulo,
+                2004,
+                libro.descripcion,
+                libro.imagen,
+                libro.archivo_url,
+                libro.isbn,
+                libro.fk_creador,
+                libro.fk_autor,
+                libro.fk_carrera,
+                libro.tipo,
+                libro.codigo,
+                libro.editorial
+               
+            ]);
+        }
+        
+
+        
     }
-  }
-  async editar(id: number, bod: string) {
-    try {
-      await this.sql.query(
-        'update libros.libro set nombre = $2 where  id_carrera = ($1)',
-        [id, bod],
-      );
-      return new MessageDto('Libro editado exitosamente');
-    } catch (error) {
-      return new NotFoundException(new MessageDto(`Error al editar el libro: ${error}`));
+
+
+
+    async eliminar(id: string) {
+
+
+        try {
+
+            
+            await this.sql.query('DELETE FROM  libros.libro WHERE  id_libro = $1;', [id])
+            return { mesage: 'eliminado ' }
+
+        } catch (error) {
+            return error
+        }
     }
-  }
+    async editar(id: Number, bod: String) {
+
+        try {
+            await this.sql.query('update libros.libro set nombre = $2 where  id_carrera = ($1)', [id, bod])
+            return "carrera actualusada  exitosamente "
+
+        } catch (error) {
+            return error
+        }
+
+    }
+
+
+
 }
