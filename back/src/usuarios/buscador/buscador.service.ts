1f82e03e148cbf2cb566fc6f3f7ed8c82a521a70
Ramen <josueskt32@gmail.com>
2024-07-12 09:48:29 -0500

    asda

--- back/src/usuarios/buscador/buscador.service.ts
+++ back/src/usuarios/buscador/buscador.service.ts
@@ -4,136 +4,81 @@ import { SqlService } from 'src/sql/sql.service';
 
 @Injectable()
 export class BuscadorService {
-  constructor(private sql: SqlService) { }
-  async buscar_libros(cadena: string, carrera: string,page:number) {
+  constructor(private sql: SqlService) {}
 
-    const pageNumber = page; 
-    const pageSize = 12; 
+  async buscar_libros(cadena: string, carrera: string, page: number) {
+    const pageNumber = page || 1;
+    const pageSize = 12;
     const offset = (pageNumber - 1) * pageSize;
-
-
-    const params = [`%${cadena}%`,pageSize, offset];
-    let restul
-    let query
+  
+    // Los parámetros de la consulta
+    let params = [`%${cadena}%`, pageSize, offset];
+    let query: string;
+    let result;
+  
     try {
+      // Consulta inicial que busca por título, ISBN y código
       query = `
-      SELECT l.id_libro, l.titulo ,l.nombre_archivo , l.year_of_publication , l.review , l.imagen  , c.nombre as nombre_carrera , a.nombre as autor_nombre , t.nombre as tipo
-      FROM libros.libro as l
-        LEFT JOIN libros.carrera as c ON l.fk_carrera = c.id_carrera
-        LEFT JOIN libros.autor as a ON l.fk_autor = a.id_autor
-        LEFT JOIN libros.tipo as t on l.fk_tipo = t.id_tipo
-        WHERE l.titulo LIKE $1 LIMIT $2 OFFSET $3
-      `;
-      restul = await this.sql.query(query, params);
-      //busca por nombre del autor 
-      if (!restul.length) {
-        query = `
-        SELECT l.id_libro, l.titulo ,l.nombre_archivo , l.year_of_publication , l.review , l.imagen , c.nombre as nombre_carrera , a.nombre as autor_nombre
+        SELECT DISTINCT l.id_libro, l.titulo, l.nombre_archivo, l.year_of_publication, l.review, l.imagen,
+                        c.nombre as nombre_carrera, a.nombre as autor_nombre, t.nombre as tipo
         FROM libros.libro as l
         LEFT JOIN libros.carrera as c ON l.fk_carrera = c.id_carrera
         LEFT JOIN libros.autor as a ON l.fk_autor = a.id_autor
-        WHERE a.nombre LIKE $1 LIMIT $2 OFFSET $3
-      `;
-        restul = await this.sql.query(query, params);
-
-      }
-      // busca por el nombre del archivo este es un test 
-      if (!restul.length) {
-        query = `
-        SELECT l.id_libro, l.titulo ,l.nombre_archivo , l.year_of_publication , l.review , l.imagen  , c.nombre as nombre_carrera , a.nombre as autor_nombre
-      FROM libros.libro as l
-        LEFT JOIN libros.carrera as c ON l.fk_carrera = c.id_carrera
-        LEFT JOIN libros.autor as a ON l.fk_autor = a.id_autor
-        WHERE l.isbn LIKE $1 LIMIT $2 OFFSET $3
-      `;
-        restul = await this.sql.query(query, params);
-      }
-      if (!restul.length) {
-        query = `
-        SELECT l.id_libro, l.titulo ,l.nombre_archivo , l.year_of_publication , l.review , l.imagen  , c.nombre as nombre_carrera , a.nombre as autor_nombre
-      FROM libros.libro as l
-        LEFT JOIN libros.carrera as c ON l.fk_carrera = c.id_carrera
-        LEFT JOIN libros.autor as a ON l.fk_autor = a.id_autor
+        LEFT JOIN libros.tipo as t ON l.fk_tipo = t.id_tipo
         LEFT JOIN libros.palabras_libro as pl ON pl.fk_libro = l.id_libro
         LEFT JOIN libros.palabras_clave as pc ON pl.fk_palabra = pc.id_palabra
-        WHERE pc.nombre LIKE $1 LIMIT $2 OFFSET $3
+        WHERE (l.titulo ILIKE $1 OR l.isbn ILIKE $1 OR l.codigo ILIKE $1  OR a.nombre ILIKE $1 OR  pc.nombre ILIKE $1)
       `;
-        restul = await this.sql.query(query, params);
-      }
-
+      
+      // Si se especifica una carrera, se añade a la consulta
       if (carrera) {
-        query += ' AND c.id_carrera = $2';
+        query += ' AND c.id_carrera = $4';
         params.push(carrera);
-        restul = await this.sql.query(query, params);
-
       }
-      return restul
+      
+      // Añadir paginación
+      query += ' LIMIT $2 OFFSET $3';
+  
+      // Ejecutar la consulta
+      result = await this.sql.query(query, params);
+  
+      return result;
     } catch (error) {
       return new MessageDto(`Error al buscar los libros, error: ${error}`);
     }
   }
 
   async index(cadena: string, carrera: string) {
-
-    
-
-
     const params = [`%${cadena}%`];
-    let restul
-    let query
+    let query: string;
+    let restul;
+
     try {
+      // Consulta inicial que busca por título, ISBN y código
       query = `
-      SELECT COUNT(*)
-      FROM libros.libro as l
-        LEFT JOIN libros.carrera as c ON l.fk_carrera = c.id_carrera
-        LEFT JOIN libros.autor as a ON l.fk_autor = a.id_autor
-        LEFT JOIN libros.tipo as t on l.fk_tipo = t.id_tipo
-        WHERE l.titulo  LIKE $1 
-      `;
-      restul = await this.sql.query(query, params);
-      //busca por nombre del autor 
-      if (!restul.length) {
-        query = `
         SELECT COUNT(*)
         FROM libros.libro as l
         LEFT JOIN libros.carrera as c ON l.fk_carrera = c.id_carrera
         LEFT JOIN libros.autor as a ON l.fk_autor = a.id_autor
-        WHERE a.nombre LIKE $1 
-      `;
-        restul = await this.sql.query(query, params);
-
-      }
-      // busca por el nombre del archivo este es un test 
-      if (!restul.length) {
-        query = `
-        SELECT COUNT(*)
-      FROM libros.libro as l
-        LEFT JOIN libros.carrera as c ON l.fk_carrera = c.id_carrera
-        LEFT JOIN libros.autor as a ON l.fk_autor = a.id_autor
-        WHERE l.isbn LIKE $1 
-      `;
-        restul = await this.sql.query(query, params);
-      }
-      if (!restul.length) {
-        query = `
-        SELECT COUNT(*)
-      FROM libros.libro as l
-        LEFT JOIN libros.carrera as c ON l.fk_carrera = c.id_carrera
-        LEFT JOIN libros.autor as a ON l.fk_autor = a.id_autor
+        LEFT JOIN libros.tipo as t ON l.fk_tipo = t.id_tipo
         LEFT JOIN libros.palabras_libro as pl ON pl.fk_libro = l.id_libro
         LEFT JOIN libros.palabras_clave as pc ON pl.fk_palabra = pc.id_palabra
-        WHERE pc.nombre LIKE $1 
+        WHERE (l.titulo ILIKE $1 OR l.isbn ILIKE $1 OR l.codigo ILIKE $1  OR a.nombre ILIKE $1 OR  pc.nombre ILIKE $1)
       `;
-        restul = await this.sql.query(query, params);
-      }
-
+      
+      // Si se especifica una carrera, se añade a la consulta
       if (carrera) {
         query += ' AND c.id_carrera = $2';
         params.push(carrera);
-        restul = await this.sql.query(query, params);
-
       }
-      return restul
+      
+      // Añadir paginación
+      
+  
+      // Ejecutar la consulta
+      restul = await this.sql.query(query, params);
+  
+      return restul;
     } catch (error) {
       return new MessageDto(`Error al buscar los libros, error: ${error}`);
     }
